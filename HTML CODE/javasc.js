75b46ccbcc127c969656451fb4f52ba35075ec64
Rixhavraj <158809245+Rixhavraj@users.noreply.github.com>
2024-11-05 16:11:08 +0530

    Add files via upload

---
+++ HTML CODE/javasc.js
@@ -0,0 +1,31 @@
+console.log('hello world');
+console.log('rishav');
+fullname="stack";
+age=55;
+x=null;
+y=undefined;
+console.log(y);
+isfollow=true;
+
+fullname=567;
+console.log(fullname);
+
+let nameFull ="ned";
+console.log(nameFull);  
+let a= 5960
+let b= 200
+c=a/b
+d=22/7*c
+console.log(c,d);
+ const student{
+    fullName:'rishavraj'
+    age:17
+    ispass:true
+ }
+
+ //use for comments
+ /*hello world
+ multiline comment
+ fgd
+ */
+
